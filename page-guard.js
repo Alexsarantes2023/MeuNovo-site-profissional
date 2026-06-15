16e928cfe648899f839e97caf3263373802b7ed4
alexarantes <alexsarantes2023@gmail.com>
2026-06-15 20:28:11 -0300

    segundo.alterado.15.06.2026

---
+++ page-guard.js
@@ -0,0 +1,26 @@
+// Lista de páginas permitidas
+const paginasPermitidas = [
+    "/",
+    "/index.html",
+    "/login.html",
+    "/pages/home/home.html",
+    "/pages/register/register.html",
+    "/dev-web-fullstack.html",
+    "/cyber-security.html",
+    "/dev-desktop-mobile.html",
+    "/lei_geral_lgpd.html"
+];
+
+// Função que verifica se a página atual é permitida
+function verificarAcesso() {
+  const caminhoAtual = window.location.pathname;
+
+  if (!paginasPermitidas.includes(caminhoAtual)) {
+    // Se não estiver na lista, redireciona ou bloqueia
+    alert("Acesso negado!");
+    window.location.href = "/index.html"; // redireciona para página segura
+  }
+}
+
+// Executa a verificação ao carregar
+verificarAcesso();
