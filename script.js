6e171f17d1b779ca831df5ce5b2986727618746e
alexarantes <alexsarantes2023@gmail.com>
2026-07-13 14:02:14 -0300

    13.07.2026

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const nav = document.querySelector(".nav");
 const registers = document.querySelector(".registerx");
 const navItens = document.querySelectorAll(".nav-itens a, .register-itens a");
 
+//Função de abrir/fechar menu
 function toggleMenu() {
+    const active = nav.classList.toggle("active");
     btnMobile.classList.toggle("active");
-    nav.classList.toggle("active");
     registers.classList.toggle("active");
 
-    const active = nav.classList.contains("active");
     btnMobile.setAttribute("aria-expanded", active);
     btnMobile.setAttribute("aria-label", active ? "Fechar Menu" : "Abrir Menu");
 }
@@ -20,42 +20,9 @@ btnMobile.addEventListener("click", toggleMenu);
 navItens.forEach((item) => {
     item.addEventListener("click", () => {
         nav.classList.remove("active");
-        register.classList.remove("active");
+        registers.classList.remove("active");
         btnMobile.classList.remove("active");
         btnMobile.setAttribute("aria-expanded", false);
         btnMobile.setAttribute("aria-label", "Abrir Menu");
     });
 });
-
-
-
-
-/*const btnMobile = document.getElementById("btn");
-const nav = document.querySelector(".nav");
-const register = document.querySelector(".register");
-//const navItens = document.querySelectorAll(".nav-itens, .register-itens");
-const navItens = document.querySelectorAll(".nav-itens a, .register-itens a");
-
-
-function toggleMenu(event) {
-    //console.log("click");
-    btnMobile.classList.toggle("active");
-    nav.classList.toggle("active");
-    register.classList.toggle("active");
-    const active = nav.classList.contains("active");
-    event.currentTarget.setAttribute("aria-expanded", active);
-    if (active) {
-        event.currentTarget.setAttribute("aria-label", "Fechar Menu");
-    } else {
-        event.currentTarget.setAttribute("aria-label", "Abrir Menu");
-    }
-}
-
-btnMobile.addEventListener("click", toggleMenu);
-
-navItens.forEach((item) => {
-    item.addEventListener("click", toggleMenu);
-});*/
-
-
-
